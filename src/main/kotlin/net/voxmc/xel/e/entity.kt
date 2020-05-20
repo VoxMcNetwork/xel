89ea97eddd3ada7df8de81c622995fc0388375e5
Sxtanna <ranald.taylor@gmail.com>
2020-05-20 09:34:28 -0400

    added entity type xelentity

---
+++ src/main/kotlin/net/voxmc/xel/e/entity.kt
@@ -0,0 +1,62 @@
+package net.voxmc.xel.e
+
+import net.voxmc.xel.c.XelModule
+import kotlin.reflect.KClass
+import kotlin.reflect.safeCast
+
+interface XelEntity
+{
+	val uuid: Long
+	
+	
+	fun loadModule(module: XelModule)
+	
+	fun killModule(module: KClass<out XelModule>): XelModule?
+	
+	
+	fun <C : XelModule> findModule(component: KClass<C>): C?
+}
+
+
+inline fun <reified C : XelModule> XelEntity.loadModule(component: C): C
+{
+	return component.also(this::loadModule)
+}
+
+inline fun <reified C : XelModule> XelEntity.loadModule(function: () -> C): C
+{
+	return function.invoke().also(this::loadModule)
+}
+
+inline fun <reified C : XelModule> XelEntity.killModule(): C?
+{
+	return this.killModule(C::class) as? C?
+}
+
+inline fun <reified C : XelModule> XelEntity.findModule(): C?
+{
+	return this.findModule(C::class)
+}
+
+
+open class XelEntityBase(final override val uuid: Long) : XelEntity
+{
+	
+	private val modules = mutableMapOf<KClass<out XelModule>, XelModule>()
+	
+	
+	override fun loadModule(module: XelModule)
+	{
+		modules[module::class] = module
+	}
+	
+	override fun killModule(module: KClass<out XelModule>): XelModule?
+	{
+		return modules.remove(module)
+	}
+	
+	override fun <C : XelModule> findModule(component: KClass<C>): C?
+	{
+		return component.safeCast(modules[component])
+	}
+}
